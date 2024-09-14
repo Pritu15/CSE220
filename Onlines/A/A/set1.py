37bb2525adb6b02fc3a027d62600ffb8beb7ed76
Pritu15 <154586403+Pritu15@users.noreply.github.com>
2024-09-14 23:25:24 +0600

    referencing problem handled

--- Onlines/A/A/set1.py
+++ Onlines/A/A/set1.py
@@ -34,9 +34,9 @@ def init_signal():
 def time_shift_signal(x : np.ndarray, k : int) -> np.ndarray:
     # implement this function
     # a=np.array(x)
-    a=np.ones(len(x))
-    np.copyto(a,x)
-    x=np.roll(x,k)
+    # a=np.ones(len(x))
+    # np.copyto(a,x)
+    # x=np.roll(x,k)
     # if k>0:
     #     x[:k]=0
         
@@ -58,39 +58,43 @@ def time_shift_signal(x : np.ndarray, k : int) -> np.ndarray:
     # #         x[i]=a[i+k]
     # #     else:
     # #         x[i]=0     
-    return x;
+    return np.roll(x,k);
 
     
 
 def time_scale_signal(x : np.ndarray, k : int) -> np.ndarray:
     # implement this function
     if k==1:
-        return x
-    mid=len(x)//2
-    a=x[:mid]
+     print(x)
+     return x
+    print("HEllo")
+    p=np.zeros(len(x))
+    np.copyto(p,x)
+    mid=len(p)//2
+    a=p[:mid]
     a=np.flip(a)
     for i in range(len(a)):
-        k=(i+1)*2
-        k=k-1
-        if k<len(a):
-            a[i]=a[k]
+        l=(i+1)*k
+        l=l-1
+        if l<len(a):
+            a[i]=a[l]
         else:
             a[i]=0
-    b=x[mid+1:]
+    b=p[mid+1:]
     for i in range(len(b)):
-        k=(i+1)*2
-        k=k-1
-        if k<len(b):
-            b[i]=b[k]
+        l=(i+1)*k
+        l=l-1
+        if l<len(b):
+            b[i]=b[l]
         else:
             b[i]=0
 
-    x[:mid]=np.flip(a)
-    x[mid+1:]=b
+    p[:mid]=np.flip(a)
+    p[mid+1:]=b
     # if(k<0):
     #     return np.flip(x)
     # return x
-    return x;
+    return p;
 
 
 def main():
